94cbc6b3543788638e7ecec707afa90d6f76b37d
yasqi-glitch <devyasminqa@gmail.com>
2024-08-28 17:03:08 +0700

    added some clickable images

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,49 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+
+import { useEffect, useState } from "react"
 import Navbar from "./components/Navbar"
 import Menubar from "./components/Menubar"
+import axios from "axios"
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [news, setNews] = useState([])
+  console.log('news: ', news);
+    useEffect(() => {
+      getNews();
+    }, []);
+  
+
+  const getNews = async () => {
+    try{
+      const response = await axios.get('https://newsapi.org/v2/everything?q=tesla&from=2024-07-28&sortBy=publishedAt&apiKey=37dedf3df9764d869a203f6e9524d5d3');
+      setNews(response.data.articles);
+    } catch (error){
+      console.error(error);
+    }
+
+  }
+
 
   return (
-   <div className="bg-gray-100 min-h-screen">
-    <Navbar/>
-   <Menubar/>
+    <div className="bg-gray-100 min-h-screen">
+      <Navbar/>
+      <Menubar/>
+      <div className="pt-5 pb-28"> 
+        {news.map((item, index) => (
+        <a key={index} href={item?.url} target="_blank">
+          <div key={index} className="bg-white p-4 border-gray-300"> 
+          <img src={item?.urlToImage} alt={item?.title} className="w-24 h-24 object-cover-rounded"></img>
+          <div>
+            <h3 className="font-semibold text-lg">{item?.title}</h3>
+            <p className="text-sm text-gray-600">{item?.description}</p>
+          </div>
+       </div>
+        </a>
+        ))}
+      </div>
    </div>
     // <>
     //   <div>
